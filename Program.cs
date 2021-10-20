83b92b847a7bef53391a51ffa8246c3aa4ceb8ec
Amoriss <amoriss@protonmail.com>
2021-10-20 10:39:07 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Roses are red, violets are blue");
+        }
+    }
+}
